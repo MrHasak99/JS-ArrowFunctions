d87e978bc8513c27187aac35fdccfc5f3086c099
MrHasak99 <hamad_alkhalaf_99@hotmail.com>
2024-11-03 20:58:29 +0300

    Finished Tasks

---
+++ arrowFunction.js
@@ -0,0 +1,24 @@
+const greeting = function (name) {
+  console.log(`Hello, ${name}!`);
+};
+greeting("Hamad");
+
+const performMath = (operation, firstNum, secondNum) =>
+  operation(firstNum, secondNum);
+
+const addition = (firstNum, secondNum) => firstNum + secondNum;
+console.log(performMath(addition, 3, 6));
+
+const square = (num) => num ** 2;
+console.log(square(7));
+
+const array = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+const arraySquared = (num) => {
+  const newArray = [];
+  for (let i = 0; i < array.length; i++) {
+    newArray[i] = num[i] ** 2;
+  }
+  return newArray;
+};
+console.log(arraySquared(array));
